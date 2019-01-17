789c36003278d6c989acd99e1587d0cfc51dfbdf
feilong4659196 <45815357+feilong4659196@users.noreply.github.com>
2019-01-17 15:23:39 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,13 +1,13 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', 'AIzaSyCVkRcSiF0KmLCwmoo4edtw0vnSykCvFcc');
+define('APIKEY', 'AIzaSyAnwjbHyLWbdGxw50eoGnFsNzKo2QG3TbA');
 define('GJ_CODE', 'US');
-define('SITE_NAME', 'y2b');
-define('TITLENAME', 'y2b');
+define('SITE_NAME', 'yanjianhua');
+define('TITLENAME', 'yanjianhua');
 define('EN2DEKEY', '32123wwerwer323tgfdgds');
-define('EMAIL', '1334347212@qq.com');
-define('NAME', 'admin');
-define('PASSWORD', 'admin');
+define('EMAIL', '349026636@qq.com');
+define('NAME', 'yanjianhua');
+define('PASSWORD', 'yanjianhua');
 define('LOGINHTML', '<!DOCTYPE html>
 <html lang="en">
 <head>
